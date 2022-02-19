4d0406b5b73beca34dbaf2752734e4fac7dea7e5
Chris Gkoumas <cgkoum01@ucy.ac.cy>
2022-02-19 15:23:26 +0200

    Added main script and images folder

---
+++ edge_detection.py
@@ -0,0 +1,77 @@
+import streamlit as st
+from PIL import Image
+import numpy as np
+import cv2
+import io
+
+
+def read_image(dir, caption=None, column_width=True):
+    read_image.image = Image.open(dir)
+    st.image(read_image.image, caption=caption, use_column_width=column_width)
+
+
+### Sidebar ###
+
+# Logo
+app_logo = Image.open("./Images/logo.png")
+st.sidebar.image(app_logo)
+
+# Image Upload
+st.sidebar.subheader("Image Upload:")
+uploaded_file = st.sidebar.file_uploader(
+    "Choose an image from your local directory...", 
+    type=["jpg", "jpeg", "png", "bmp"]
+)
+
+# Thresholds
+st.sidebar.subheader("Apply thresholding:")
+low_thresh = st.sidebar.number_input(
+    "Insert a number for the lower threshold (default=15)."
+)
+upper_thresh = st.sidebar.number_input(
+    "Insert a number for the upper threshold (default=200)."
+)
+
+# Detect edges button
+edge_button = st.sidebar.button("Click to detect edges")
+
+# App development info
+st.sidebar.markdown(
+    "Application made by Christos Gkoumas" 
+    "([@n3uro_virtual](https://twitter.com/n3uro_virtual)). Version 0.1.0"
+)
+
+
+### Main App ###
+
+# Main title
+st.title("Edge Detection Application")
+
+# Original image
+if uploaded_file is None:
+    read_image("./Images/demo_image.jpg", caption="Demo Image")
+else:
+    read_image(uploaded_file, caption="Uploaded Image")
+
+# Edge Image
+if edge_button:
+    array = np.asarray(read_image.image)
+    gray = cv2.cvtColor(array, cv2.COLOR_BGR2GRAY)
+    blurry = cv2.GaussianBlur(gray, (5, 5), 0)
+    if low_thresh == 0 and upper_thresh == 0:
+        edged = cv2.Canny(blurry, 15, 200)
+    else:
+        edged = cv2.Canny(blurry, low_thresh, upper_thresh)
+    st.image(edged, caption="Edge Image", use_column_width=True)
+    
+    # Download button
+    im = Image.fromarray(edged)
+    im.save("Edge_image.png")
+    with open("Edge_image.png", "rb") as file:
+        btn = st.download_button(
+                 label="Download Image",
+                 data=file,
+                 file_name="Edge_image.png",
+                 mime="image/png"
+               )
+    
